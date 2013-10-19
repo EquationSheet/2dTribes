57e82699d7bdbdc90f0c02c8cd06225f13c3d200
ugotnitined <nitin.sadras@gmail.com>
2013-10-18 21:59:43 -0700

    fullscreen canvas

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
