a5dde8e0ed5b17cb74e7f260a47ab0f0ffcec798
JonghyunAhn <jonghyunahn.6@berkeley.edu>
2013-10-19 03:05:06 -0700

    game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,33 +14,25 @@ function World() {
         return context['identifier']
     }
 
-    this.getRelativeCoordinateFilter=function(context) {
-        var currCoord = this.getXYCoord(context);
-        var players = this.getPlayers(context);
-        var selfID = this.IDKey(context);
-        for (var player in players) {
-            if (player.identifier == selfID) {
-                players.remove(player);           
-            }
-            else if (Math.abs(player.x-currCoord.x) > screen.width/2 || Math.abs(player.y-currCoord.y) > screen.height/2) {
-                players.remove(player)
-            }
-            else {
-                player.x = canvas.width/2 + player.x-currCoord.x
-                player.y = canvas.height/2 + player.y-currCoord.y
-            }
-        }
-        return players
-    }
-
     this.draw=function(context) {
-        playersToDraw = this.getRelativeCoordinateFilter(context);
+        var playersToDraw = this.getPlayers(context);
+        var currCoord = this.getXYCoord(context);
         var canvas = Raphael(screen.width/2,screen.height/2,screen.width,screen.height)
 
         for (var player in playersToDraw) {
-            var circle = canvas.circle(player.x,player.y,20);
-            circle.attr("fill",'#f00');
-            circle.attr("stroke","#fff");
+            if (player.identifier == selfID) {
+                var circle = canvas.circle(screen.width/2,screen.height/2,20);
+                circle.attr("fill",'#00f');
+                circle.attr('stroke','#fff');
+            }
+            else if (Math.abs(player.x-currCoord.x) > screen.width/2 || Math.abs(player.y-currCoord.y) > screen.height/2) {
+                continue;
+            }
+            else {
+                var circle = canvas.circle(currCoord.x - player.x,currCoord.y - player.y,20);
+                circle.attr("fill",'#f00');
+                circle.attr("stroke","#fff");
+            }
         }
     }
 }
